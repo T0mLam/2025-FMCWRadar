4216d98f3eb356f14a214ac482b15a5c3398284a
Dan <ff24459@bristol.ac.uk>
2026-04-07 10:02:35 +0100

    Added function for cam tab to receive radar data

--- Applications_Visualizer/common/camera_tab.py
+++ Applications_Visualizer/common/camera_tab.py
@@ -4,6 +4,8 @@ from PySide2.QtGui import QImage, QPixmap
 import cv2
 import sys
 
+from ultralytics import YOLO
+
 class CameraTab(QWidget):
     def __init__ (self, parent=None):
         super().__init__(parent)
@@ -24,7 +26,15 @@ class CameraTab(QWidget):
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_frame)
 
+        self.yolo_model = YOLO('yolov8n.pt')
+
+        self.latest_radar_data = None
+
     
+    def receive_radar_data(self, outputDict):
+        self.latest_radar_data = outputDict
+
+
     def start_camera(self):
         # Check version to see if we need to use DSHOW (Alina this is what was causing the crash as well as needing the headless version)
         if self.capture is None:
@@ -65,6 +75,13 @@ class CameraTab(QWidget):
 
                 frame = cv2.resize(frame, (self.display_width, self.display_height))
                 h, w, ch = frame.shape
+
+                results = self.yolo_model(frame, classes=[0], verbose=False)
+
+                cv_boxes = [] 
+                for box in results[0].boxes:
+                    x1, y1, x2, y2 = map(int,box.xyxy[0])
+                    cv_boxes.append((x1,y1,x2,y2))
 
                 bytes_per_line = ch * w
 
